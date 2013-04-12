e9799d9edce848095e1a217dc6d65fb5214cc464
Arne Jørgensen <arne@arnested.dk>
2013-04-12 07:52:32 +0200

    Carton 0.2.0

--- Formula/carton.rb
+++ Formula/carton.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Carton < Formula
   homepage 'https://github.com/rejeep/carton'
-  url 'https://github.com/rejeep/carton/archive/v0.1.2.zip'
-  sha1 '2c811658ff10e05e7fdbc45ec2310f3610545c9f'
+  version '0.2.0'
+  url 'https://github.com/rejeep/carton.git', :tag => "v#{version}"
   head 'https://github.com/rejeep/carton.git'
 
   def install
