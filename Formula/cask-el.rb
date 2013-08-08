646bfea401b3fc43c761ab9ee98f6f2ca107e2ac
Arne Jørgensen <arne@arnested.dk>
2013-08-08 11:31:16 +0200

    Carton was renamed to Cask upstream.

---
+++ Formula/cask-el.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class CaskEl < Formula
+  homepage 'https://github.com/rejeep/cask.el'
+  version '0.4.2'
+  url 'https://github.com/rejeep/cask.el.git', :tag => "v#{version}"
+  head 'https://github.com/rejeep/cask.el.git'
+
+  def install
+    system "rm", "bin/carton"
+    prefix.install Dir["*"]
+  end
+
+  def test
+    Dir.chdir "#{prefix}"
+    system "make", "ecukes"
+  end
+end
