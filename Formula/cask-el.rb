2f096c4c1f7f3d587bc011ca306bc9a47ef00256
Arne Jørgensen <arne@arnested.dk>
2013-09-04 22:59:17 +0200

    Carton 0.4.6

--- Formula/cask-el.rb
+++ Formula/cask-el.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class CaskEl < Formula
   homepage 'https://github.com/rejeep/cask.el'
-  version '0.4.2'
+  version '0.4.6'
   url 'https://github.com/rejeep/cask.el.git', :tag => "v#{version}"
   head 'https://github.com/rejeep/cask.el.git'
 
