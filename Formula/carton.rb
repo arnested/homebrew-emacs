2887d75413dcec0b44f9bb11f8846df32c3c0632
Arne Jørgensen <arne@arnested.dk>
2013-09-04 22:55:38 +0200

    Fixed Carton formula for use in non-updated projects.

--- Formula/carton.rb
+++ Formula/carton.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Carton < Formula
-  homepage 'https://github.com/rejeep/carton'
+  homepage 'https://github.com/rejeep/cask.el'
   version '0.3.1'
-  url 'https://github.com/rejeep/carton.git', :tag => "v#{version}"
-  head 'https://github.com/rejeep/carton.git'
+  url 'https://github.com/rejeep/cask.el.git', :tag => "v#{version}"
+  head 'https://github.com/rejeep/cask.el.git'
 
   def install
     prefix.install Dir["*"]
@@ -13,5 +13,17 @@ class Carton < Formula
   def test
     Dir.chdir "#{prefix}"
     system "make", "all"
+  end
+
+  def caveats; <<-EOS.undent
+    Carton changed its name to cask.el.
+
+    See https://github.com/rejeep/cask.el#migration-from-carton on how to migrate
+    from Carton to Cask.
+
+    This formula install the last version of Carton (v0.3.1).
+
+    To install Cask use the cask-el formula from the arnested/emacs tap.
+    EOS
   end
 end
--- README.md
+++ README.md
@@ -12,10 +12,15 @@ When you get tired of me breaking stuff uninstall with:
 
 ## Formulas
 
- * [`carton`](Formula/carton.rb) - _Emacs dependency management made easy_
+ * [`cask-el`](Formula/cask-el.rb) - _Emacs dependency management made easy_
  
-   A formula for installing [carton](https://github.com/rejeep/carton).
+   A formula for installing [cask](https://github.com/rejeep/cask.el).
    
+ * [`carton`](Formula/carton.rb) - _Old version of Cask (before the renaming)_
+
+   Install version 0.3.1 of Carton (now Cask). In case you need it for
+   projects that haven't migrated yet,
+
 There used to be a `emacs-mac` formula here as well. I removed it and
 instead refer you to the "official" formula:
 
