bb18699aee819ef37c76685e4ccb0c0d8150c1eb
Arne Jørgensen <arne@arnested.dk>
2014-03-13 00:31:28 +0100

    Deprecate global-patched.
    GNU Global now has the patches build in.

--- Formula/global-patched.rb
+++ Formula/global-patched.rb
@@ -1,40 +1,14 @@
-# -*- coding: utf-8 -*-
-require File.join(File.dirname(__FILE__), 'global')
+require 'formula'
 
-class GlobalPatched < Global
+class GlobalPatched < Formula
 
   homepage 'https://gist.github.com/arnested/8971896'
+  url 'http://ftpmirror.gnu.org/global/global-6.2.11.tar.gz'
+  sha1 'b0f50213680ec3288988354c34e3b3ae1a42719e'
 
-  def initialize name='__UNKNOWN__', path=nil
-    g = Global.new name, path
-    @stable = g.stable
-    @devel = g.devel
-    @head = g.head
-    @bottle = g.bottle
-    super name, path
-  end
+  depends_on 'global'
 
-  conflicts_with 'global'
-
-  def patches
-    { :p0 => [ 'https://gist.github.com/arnested/8971896/raw/c673d9939d9f14ac9bfe9eb008abb2260741ef2b/glob.patch',
-               'https://gist.githubusercontent.com/arnested/8971896/raw/2ee412fb7eca2c33a0fe94a687937bb36dfd80b7/gtags-conf.patch' ] }
-   end
-
-  def caveats; <<-EOS.undent
-    This version is patched to allow you to put glob patterns in your
-    skip lists.
-
-    The supported glob patterns are:
-
-      *, ?, [...], [!...], [^...]
-      ('!' and '^' means 'not')
-
-    This version is patched to integrate better with Emacs flymake
-    inplace temporary files (*_flymake.*,*_flymake) and Emacs backup
-    files (*~).
-
-    See #{homepage}
-    EOS
+  def install
+    # intentionally blank
   end
 end
